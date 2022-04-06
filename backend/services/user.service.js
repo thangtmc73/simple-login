42e15a1fa92a7b3802896da62acb979f18f869ff
thangtm2 <thangtm2@vng.com.vn>
2022-04-07 03:56:18 +0700

    feat: validate username and password in user service

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,7 +1,13 @@
-// greeter.service.js
+const DbMixin = require("../mixins/db.mixin");
+const { MoleculerError } = require("moleculer").Errors;
+
 module.exports = {
 	name: "user",
-
+	mixins: [DbMixin("users")],
+	settings: {
+		/** Public fields */
+		fields: ["returncode", "returnmessage"],
+	},
 	actions: {
 		login: {
 			rest: {
@@ -13,8 +19,33 @@ module.exports = {
 				password: "string"
 			},
 			async handler(ctx) {
-				return `Welcome, ${ctx.params.username} with ${ctx.params.password}`;
+				const { username, password } = ctx.params;
+				const matchUsers =  await this.adapter.find({ query: { username } });
+				console.log("matchUsers", matchUsers);
+				if (!Array.isArray(matchUsers) || !matchUsers.length) {
+					return { returncode: -1, returnmessage: "The user is not existed" };
+				}
+				const [user] = matchUsers;
+				if (password !== user.password) {
+					return { returncode: -2, returnmessage: "The password is wrong" };
+				}
+				return { returncode: 1, returnmessage: "Success" };
 			}
 		},
-	}
+	},
+
+	methods: {
+		/**
+		 * Loading sample data to the collection.
+		 * It is called in the DB.mixin after the database
+		 * connection establishing & the collection is empty.
+		 */
+		async seedDB() {
+			await this.adapter.insertMany([
+				{"username":"admin","password":"123456"},
+				{"username":"thangtm","password":"654321"}
+			]);
+		}
+	},
+
 };
