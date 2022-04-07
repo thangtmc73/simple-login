1e6b5fd620a7337e6dd7a790db4189a8cd039b11
thangtm2 <thangtm2@vng.com.vn>
2022-04-07 13:27:51 +0700

    feat(frontend): update logic login

--- frontend/src/Login/index.js
+++ frontend/src/Login/index.js
@@ -1,28 +1,48 @@
+import { useState } from "react";
+import {
+  useNavigate
+} from "react-router-dom";
 import "./Login.scss";
 import useInputChange from "../hooks/useInputChange";
-import login from "../api/login";
+import useAuthen from "../hooks/useAuthen";
 
 function Login() {
   const [userName, handleUserNameChange] = useInputChange("");
   const [password, handlePasswordChange] = useInputChange("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const { signIn } = useAuthen();
+  const navigate = useNavigate();
 
   function handleLoginClick(e) {
     e.preventDefault();
-    login({ userName, password });
+    signIn({
+      userName, password
+    }, () => {
+      navigate("/", { replace: true });
+    }, (error) => {
+      setErrorMessage(error);
+    });
   }
 
   return (
     <form className="login">
+      {errorMessage && <span className="error">{errorMessage}</span>}
       <input
         placeholder="Username"
         value={userName}
-        onChange={handleUserNameChange}
+        onChange={(e) => {
+          setErrorMessage("");
+          handleUserNameChange(e);
+        }}
       />
       <input
         placeholder="Password"
         type="password"
         value={password}
-        onChange={handlePasswordChange}
+        onChange={(e) => {
+          setErrorMessage("");
+          handlePasswordChange(e);
+        }}
       />
       <button onClick={handleLoginClick}>Login</button>
     </form>
