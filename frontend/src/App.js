63cd3f89828cd1a01e56532d13f22ab155a8944c
thangtm2 <thangtm2@vng.com.vn>
2022-04-07 13:31:49 +0700

    feat: apply routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import UserStatus from "./components/UserStatus";
+import AuthenProvider from "./AuthenProvider";
+import RequiredAuthenWrapper from './components/RequiredAuthenWrapper';
+import Default from "./Default";
 import Login from "./Login";
+import User from "./UserInfo";
 function App() {
   return (
-    <Login />
+    <AuthenProvider>
+      <div className="App">
+        <UserStatus />
+        <p>Welcome</p>
+        <Routes>
+          <Route path="/" element={<Default />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user" element={<RequiredAuthenWrapper><User /></RequiredAuthenWrapper>} />
+        </Routes>
+      </div>
+    </AuthenProvider>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +9,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
